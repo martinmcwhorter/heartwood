48d6c785fcd2f991e892adb14abbf0a4771fb35c
Martin McWhorter <martin.mcwhorter@verizonconnect.com>
2021-12-19 12:39:29 +0000

    feat: added disabled to buttons

--- example/src/main.rs
+++ example/src/main.rs
@@ -9,6 +9,8 @@ fn main() {
 
     let clicked = Signal::new(false);
     let clicked_count = Signal::new(0);
+    let disable_all = Signal::new(false);
+    let disable_click = Signal::new(false);
 
     create_effect({
         let clicked = clicked.clone();
@@ -20,29 +22,51 @@ fn main() {
         }
     });
 
+    create_effect({
+        let disable_click = disable_click.clone();
+        let disable_all = disable_all.clone();
+        move || {
+            if *disable_click.get() {
+                if *disable_all.get() {
+                    disable_all.set(false);
+                } else {
+                    disable_all.set(true)
+                }
+            }
+        }
+    });
+
+    let enable_disable_label = create_selector(cloned!((disable_all) => move || 
+        if *disable_all.get() { "DISABLE ALL".to_string() } else { "ENABLE ALL".to_string() } ));
+
     let value = Signal::new("".to_string());
 
     sycamore::render(|| {
         view! {
 
             Button(ButtonProps {
-                variant: ButtonVariant::Contained,
-                label: "CONTAINED BUTTON",
-                on_click: clicked.clone()
+                label: Signal::new("CONTAINED BUTTON".to_string()).handle(),
+                on_click: clicked.clone(),
+                disabled: disable_all.clone(),
+                ..ButtonProps::default()
             })
             br()
 
             Button(ButtonProps {
                 variant: ButtonVariant::Outlined,
-                label: "OUTLINED BUTTON",
-                on_click: clicked.clone()
+                label: Signal::new("OUTLINED BUTTON".to_string()).handle(),
+                on_click: clicked.clone(),
+                disabled: disable_all.clone(),
+                ..ButtonProps::default()
             })
             br()
 
             Button(ButtonProps {
                 variant: ButtonVariant::Text,
-                label: "TEXT BUTTON",
-                on_click: clicked.clone()
+                label: Signal::new("TEXT BUTTON".to_string()).handle(),
+                on_click: clicked.clone(),
+                disabled: disable_all.clone(),
+                ..ButtonProps::default()
             })
             br()
 
@@ -61,6 +85,14 @@ fn main() {
                 "text field value: "
                 (value.get())
             }
+
+            br()br()
+            Button(ButtonProps {
+                label: enable_disable_label,
+                on_click: disable_click.clone(),
+                ..ButtonProps::default()
+            })
+            br()
 
         }
     });
--- src/button.rs
+++ src/button.rs
@@ -4,16 +4,30 @@ use sycamore::rt::JsCast;
 use web_sys::Event;
 use web_sys::HtmlElement;
 
+#[derive(Clone)]
 pub enum ButtonVariant {
     Contained,
     Outlined,
     Text,
 }
 
+#[derive(Clone)]
 pub struct ButtonProps {
     pub variant: ButtonVariant,
-    pub label: &'static str,
+    pub label: ReadSignal<String>,
     pub on_click: Signal<bool>,
+    pub disabled: Signal<bool>,
+}
+
+impl Default for ButtonProps {
+    fn default() -> Self {
+        Self {
+            variant: ButtonVariant::Contained,
+            label: Signal::new("".to_string()).handle(),
+            on_click: Signal::new(false),
+            disabled: Signal::new(false),
+        }
+    }
 }
 
 #[component(Button<G>)]
@@ -31,12 +45,12 @@ pub fn button(props: ButtonProps) -> View<G> {
     let button_classes = format!("mdc-button {} mdc-button--touch", variant);
 
     view! {
-        button(class=button_classes, on:click=handle_click) {
+        button(class=button_classes, on:click=handle_click, disabled=*props.disabled.get()) {
             span(class="mdc-button__ripple")
             span(class="mdc-button__touch")
             span(class="mdc-button__focus-ring")
             span(class="mdc-button__label") {
-                (props.label)
+                (*props.label.get())
             }
         }
     }
